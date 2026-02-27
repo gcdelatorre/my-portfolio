a26bba2492860f4589067d6eba72250b351662a6
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2026-02-28 00:01:59 +0800

    fix fonts

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -1,9 +1,9 @@
 export default function About() {
     return (
         <div className="flex flex-col md:flex-col items-start md:items-start justify-start gap-3 w-full py-12">
-            <h1 className="text-xl md:text-[26px] tracking-normal text-primary leading-tight font-sans font-semibold">About</h1>
-            <p className="text-sm md:text-[16px] text-foreground tracking-wide font-sans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non cupiditate ullam rem iure ut! Tempore minus nesciunt incidunt, magnam mollitia laudantium? Quidem maiores facere recusandae dolore delectus cum quo nobis!</p>
-            <p className="text-sm md:text-[16px] text-foreground tracking-wide font-sans">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quisquam porro doloremque cumque id? Sit neque optio maiores tempora fugiat.</p>
+            <h1 className="text-xl md:text-[26px] tracking-normal text-primary leading-tight font-clash font-[600]">About</h1>
+            <p className="text-sm md:text-[16px] text-foreground tracking-wide font-sans font-[400]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non cupiditate ullam rem iure ut! Tempore minus nesciunt incidunt, magnam mollitia laudantium? Quidem maiores facere recusandae dolore delectus cum quo nobis!</p>
+            <p className="text-sm md:text-[16px] text-foreground tracking-wide font-sans font-[400]">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quisquam porro doloremque cumque id? Sit neque optio maiores tempora fugiat.</p>
         </div>
     )
 }
