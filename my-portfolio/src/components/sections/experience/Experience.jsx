df6f2b7787280fad6131aac1657479ae309f9e5d
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2026-03-02 23:14:01 +0800

    re label experiencens

--- my-portfolio/src/components/sections/experience/Experience.jsx
+++ my-portfolio/src/components/sections/experience/Experience.jsx
@@ -3,7 +3,7 @@ import { ExperienceAccordions } from "./ExperienceAccordions"
 export default function Experience() {
     return (
         <div className="flex flex-col items-start justify-start gap-4 w-full pt-12">
-            <h1 className="text-xl md:text-[26px] tracking-normal text-primary leading-tight font-clash font-[600]">Experience</h1>
+            <h1 className="text-xl md:text-[26px] tracking-normal text-primary leading-tight font-clash font-[600]">Projects</h1>
             <ExperienceAccordions />
         </div>
     )
